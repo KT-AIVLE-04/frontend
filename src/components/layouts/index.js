a48295b026e5d026bd88392384f0ebbae57804b9
charmeee <mandugohigh@gmail.com>
2025-08-24 22:18:41 +0900

    feat: 디폴트 컴포넌트를추가할수잇는 api 컴포넌트추가

--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -1,4 +1,5 @@
 export { ApiPageLayout } from './ApiPageLayout';
+export { ApiStateLayout } from './ApiStateLayout';
 export { DataListLayout } from './DataListLayout';
 export { FormPageLayout } from './FormPageLayout';
 
