01791bbcf9da5c878bcf39041646d5a0686c76ee
charmeee <mandugohigh@gmail.com>
2025-08-24 22:30:26 +0900

    remove: dashboard삭제

--- src/pages/Dashboard/components/index.js
+++
@@ -1,5 +0,0 @@
-export { ActivityItem } from './ActivityItem';
-export { StatCard } from './StatCard';
-export { createStatCard } from './StatCardCreator';
-export { TrendSection } from './TrendSection';
-
